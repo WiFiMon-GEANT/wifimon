9dc295255c9af00bb4fcb3ac95a108209d2f9e60
Nikos Kanakis <full290@gmail.com>
2016-02-23 10:17:56 +0200

    ssl client

--- wifimon-ui/src/main/java/net/geant/wifimon/agent/AgentConfiguration.java
+++ wifimon-ui/src/main/java/net/geant/wifimon/agent/AgentConfiguration.java
@@ -12,10 +12,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
 import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.Map;
 
 /**
@@ -30,19 +36,35 @@ public class AgentConfiguration {
 
     @Bean
     public Client client() {
-        ClientConfig clientConfig = new DefaultClientConfig();
-        clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
-
+        TrustManager[ ] certs = new TrustManager[ ] {
+                new X509TrustManager() {
+                    @Override
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return null;
+                    }
+                    @Override
+                    public void checkServerTrusted(X509Certificate[] chain, String authType)
+                            throws CertificateException {
+                    }
+                    @Override
+                    public void checkClientTrusted(X509Certificate[] chain, String authType)
+                            throws CertificateException {
+                    }
+                }
+        };
         SSLContext sslcontext = null;
         try {
             sslcontext = SSLContext.getInstance("TLS");
-            sslcontext.init(null, null, null);
+            sslcontext.init(null, certs, new SecureRandom());
         } catch (NoSuchAlgorithmException | KeyManagementException e) {
             e.printStackTrace();
         }
-        Map<String, Object> properties = clientConfig.getProperties();
-        HTTPSProperties httpsProperties = new HTTPSProperties((s, sslSession) -> true, sslcontext);
-        properties.put(HTTPSProperties.PROPERTY_HTTPS_PROPERTIES, httpsProperties);
+        HttpsURLConnection.setDefaultSSLSocketFactory(sslcontext.getSocketFactory());
+        ClientConfig clientConfig = new DefaultClientConfig();
+        clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
+        clientConfig.getProperties().
+                put(HTTPSProperties.PROPERTY_HTTPS_PROPERTIES,
+                        new HTTPSProperties((hostname, session) -> true,sslcontext));
 
         return Client.create(clientConfig);
     }
