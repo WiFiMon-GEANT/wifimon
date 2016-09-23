34bdde7ff84420311909b6c47338d3530352fc19
Vasileios Kokkinos <kokkinos@cti.gr>
2016-09-23 12:36:38 +0300

    WIFIM-6	Secure processor ew file:   wifimon-secure-processor/README.txt

---
+++ wifimon-secure-processor/src/main/java/net/geant/wifimon/secureprocessor/repository/RadiusRepository.java
@@ -0,0 +1,20 @@
+package net.geant.wifimon.secureprocessor.repository;
+import net.geant.wifimon.model.entity.Radius;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Date;
+import java.util.Optional;
+
+/**
+ * Created by kokkinos on 2/9/2016.
+ */
+
+public interface RadiusRepository extends PagingAndSortingRepository<Radius, Long> {
+
+    Optional<Radius> findFirst1ByFramedIpAddressOrderByStopTimeAsc(String framedIpAddress);
+
+    @Query(value = "SELECT DISTINCT ON (framedipaddress) radacct.* FROM radacct WHERE abbrev(framedipaddress) = :client_ip AND acctstoptime IS NULL OR acctstoptime < acctstarttime AND acctstarttime < :date ORDER BY framedipaddress, acctstarttime DESC", nativeQuery = true)
+    Radius find(@Param("client_ip") String clientIp, @Param("date") Date date);
+}
---
+++ wifimon-secure-processor/src/main/java/net/geant/wifimon/secureprocessor/repository/SubnetRepository.java
@@ -0,0 +1,13 @@
+package net.geant.wifimon.secureprocessor.repository;
+import net.geant.wifimon.model.entity.Subnet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+/**
+ * Created by kokkinos on 2/9/2016.
+ */
+public interface SubnetRepository extends JpaRepository<Subnet, Long> {
+
+    List findBySubnet(String subnet);
+
+}
