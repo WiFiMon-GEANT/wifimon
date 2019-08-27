aba2eab8873a8709624f09d0ee5729b35b3e6850
kostopoulos.ntua <nkostopoulos@netmode.ntua.gr>
2019-08-27 16:10:04 +0000

    included wifimon configuration file

---
+++ wifimon/wifimon-agent-start.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+java -jar /home/wifimon/wifimon/secure-processor-0.1.1.jar --spring.config.location=classpath:/home/wifimon/wifimon/config/secure-processor.properties,file:/home/wifimon/wifimon/config/secure-processor.properties &
+
+
