4bb1c9e9fad36bb579bdcd63e43499237849b248
root <root@wifi-test2.novalocal>
2020-09-07 10:09:28 +0200

    version 1.1.0

--- wifimon-assembly/start.sh
+++ wifimon-assembly/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-java -jar /usr/lib/wifimon/ui-0.1.1-SNAPSHOT.war --spring.config.location=classpath:/usr/lib/wifimon/config/ui.properties,file:/usr/lib/wifimon/config/ui.properties &
-java -jar /usr/lib/wifimon/secure-processor-0.1.1-SNAPSHOT.war --spring.config.location=classpath:/usr/lib/wifimon/config/secure-processor.properties,file:/usr/lib/wifimon/config/secure-processor.properties &
+java -jar /usr/lib/wifimon/ui-1.1.0-SNAPSHOT.war --spring.config.location=classpath:/usr/lib/wifimon/config/ui.properties,file:/usr/lib/wifimon/config/ui.properties &
+java -jar /usr/lib/wifimon/secure-processor-1.1.0-SNAPSHOT.war --spring.config.location=classpath:/usr/lib/wifimon/config/secure-processor.properties,file:/usr/lib/wifimon/config/secure-processor.properties &
 
 
