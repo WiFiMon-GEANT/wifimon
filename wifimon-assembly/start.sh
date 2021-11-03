f6c0abaa896835b3ed0a5ea8b023c3b052c87f32
root <root@wifimon-test.netmode.ece.ntua.gr>
2021-11-03 16:55:29 +0200

    v1.4.2 code dependencies

--- wifimon-assembly/start.sh
+++ wifimon-assembly/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-java -jar /usr/lib/wifimon/ui-1.4.1.war --spring.config.location=classpath:/usr/lib/wifimon/config/ui.properties,file:/usr/lib/wifimon/config/ui.properties &
-java -jar /usr/lib/wifimon/secure-processor-1.4.1.war --spring.config.location=classpath:/usr/lib/wifimon/config/secure-processor.properties,file:/usr/lib/wifimon/config/secure-processor.properties &
+java -jar /usr/lib/wifimon/ui-1.4.2.war --spring.config.location=classpath:/usr/lib/wifimon/config/ui.properties,file:/usr/lib/wifimon/config/ui.properties &
+java -jar /usr/lib/wifimon/secure-processor-1.4.2.war --spring.config.location=classpath:/usr/lib/wifimon/config/secure-processor.properties,file:/usr/lib/wifimon/config/secure-processor.properties &
