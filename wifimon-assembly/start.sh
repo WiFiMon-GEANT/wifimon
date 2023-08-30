5dd9a49af359c3f246e6afe3c1dac8a6fb8feba6
root <root@penny.netmode.ece.ntua.gr>
2023-08-30 17:39:42 +0300

    updated Jersey, Spring Boots version and start.sh for Spring 3

--- wifimon-assembly/start.sh
+++ wifimon-assembly/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-java -jar /usr/lib/wifimon/ui-2.1.0.war --spring.config.location=classpath:/usr/lib/wifimon/config/ui.properties,file:/usr/lib/wifimon/config/ui.properties &
-java -jar /usr/lib/wifimon/secure-processor-2.1.0.war --spring.config.location=classpath:/usr/lib/wifimon/config/secure-processor.properties,file:/usr/lib/wifimon/config/secure-processor.properties &
+java -jar /usr/lib/wifimon/ui-2.1.0.war --spring.config.location=/usr/lib/wifimon/config/ui.properties,file:/usr/lib/wifimon/config/ui.properties &
+java -jar /usr/lib/wifimon/secure-processor-2.1.0.war --spring.config.location=/usr/lib/wifimon/config/secure-processor.properties,file:/usr/lib/wifimon/config/secure-processor.properties &
